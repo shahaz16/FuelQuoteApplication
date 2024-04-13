c3609833f94d10e0aa21fe4f000e4ec313ca6949
shahaz16 <65368607+shahaz16@users.noreply.github.com>
2024-04-12 21:47:27 -0500

    added mongodb login and code

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 import express from "express";
-import cors from 'cors'
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import cpmRoutes from "./routes/cpmRoutes.js";
 import historyRoutes from "./routes/historyRoutes.js";
-
+import mongoose from 'mongoose'
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+
+const connect= async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+    } catch (error) {
+        throw error
+    }
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,5 +33,6 @@ app.use("/api", historyRoutes);
 if (process.env.NODE_ENV !== "test") {
     app.listen(process.env.PORT || 5500);
     console.log("Connected to Server"); 
+    connect();
 }
 export default app;
