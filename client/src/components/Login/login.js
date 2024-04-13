d7be0ce50d1b27b4d693c60fd675d5189c348c16
shahaz16 <65368607+shahaz16@users.noreply.github.com>
2024-04-12 22:12:24 -0500

    login connected to db

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,11 +1,56 @@
-import React from "react";
-
-import { useNavigate } from 'react-router-dom';
 import "./login.css";
+import React from "react";
+import { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
   const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    Username: "",
+    Password: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5500/api/login", credentials);
+      localStorage.setItem("user", JSON.stringify(response.data));
+
+      if (response.status === 200) {
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        const response = axios.get("http://localhost:5500/api/getCPMById/" + user.cpm_id)
+          .then((response) => {
+            localStorage.setItem("cpm", JSON.stringify(response.data));
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
+
+        alert("Login Successful. Press OK to continue.");
+        navigate("/");
+      }
+      else {
+        alert("Our server indicates that you have not set up your profile yet. Press OK to continue.");
+        navigate("/cpm");
+      }
+    } catch (err) {
+      if (err.response.status === 400) {
+        alert("Invalid username or password. Please try again.");
+      }
+    }
+  };
 
   return (
     <div className="form">
@@ -17,6 +62,7 @@ const Login = () => {
           id="Username"
           className="rInput"
           maxLength="50"
+          onChange={handleChange}
           required
         />
       </label>
@@ -31,17 +77,14 @@ const Login = () => {
           id="Password"
           className="rInput"
           maxLength="100"
+          onChange={handleChange}
           required
         />
       </label>
       <br />
       <br />
 
-      <button className="Login" 
-      onClick={() => {
-        navigate("/cpm");
-      }}
-      >Login</button>
+      <button className="Login" onClick={handleLogin}>Login</button>
       <br />
       <br />Don't have an account?
       <button
